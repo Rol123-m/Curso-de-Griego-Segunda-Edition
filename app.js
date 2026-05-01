8d7f46a5e3deae6e586ef5c24c7ebac131357d04
Rol123-m <rol123-m.github.io.com>
2026-05-01 17:38:51 -0400

    hjdh

--- app.js
+++ app.js
@@ -199,7 +199,8 @@ const ESTUDIANTES_POR_GRUPO = {
         { id: 46, nombre: "Emanuel Tito Guerrero", email: "titoemanuel03@gmail.com", telefono: "51464558", nacionalidad: "Cuba" },
         { id: 47, nombre: "Janet Vargas Cuevas", email: "janetvargascuevas155@gmail.com", telefono: "52998335", nacionalidad: "Cuba" },
         { id: 71, nombre: "Yaquelín Ramírez", email: "ramirezcuadradoyaqulin@gmail.com", telefono: "56511185", nacionalidad: "Cuba" },
-        { id: 74, nombre: "Ernesto Jiménez del Risco", email: "ernestjr16499@gmail.com", telefono: "58917573", nacionalidad: "Cuba" }
+        { id: 74, nombre: "Ernesto Jiménez del Risco", email: "ernestjr16499@gmail.com", telefono: "58917573", nacionalidad: "Cuba" },
+         { id: 75, nombre: "Melba Gainza", email: "ernestjr16499@gmail.com", telefono: "58917573", nacionalidad: "Cuba" }
     ],
     
     GAMMA: [
